f302ed1c7c73691383c06f86a84c48f50c5e837f
ArielGad <ariel.gad4@gmail.com>
2020-06-17 22:35:59 +0300

    Add feature to list tags

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import \
 
 from django.conf import settings
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
